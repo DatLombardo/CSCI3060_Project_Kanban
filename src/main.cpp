1f1d1551c1ecb6b1fc3b4402b4904f555616f845
Lachlan Johnston <lachlan.johnston@uoit.net>
2018-02-24 23:02:27 -0500

    fixed logic error

--- src/main.cpp
+++ src/main.cpp
@@ -22,14 +22,7 @@ void main() {
         std::cout << "Please enter a transaction code: ";
         std::cin >> userInput;
         
-        if (loggedIn) {
-            transaction t = transaction(userInput, currentUser); // transaction data input lookp
-            
-            if (t.valid)
-                transactions.append(t);
-            else
-                continue; // transaction failed; class will print error
-        } else if (userInput == "logout" && loggedIn) {
+        if (userInput == "logout" && loggedIn) {
             // user has chose to logout, write transactions to appropriate file(s)
             
             fileout f = new fileout();
@@ -37,12 +30,21 @@ void main() {
             tranasctions.append(transaction(userInput));
             f.writetransactions(transactions);
         
+        } else if (loggedIn) {
+            transaction t = transaction(userInput, currentUser); // transaction data input lookp
+            
+            if (t.valid)
+                transactions.append(t);
+            else
+                continue; // transaction failed; class will print error
         } else {
             if (userInput == "login") {
                 transaction t = transaction(userInput);
                 
                 loggedIn = true;
                 user = t.currentUser;
+
+                transactions.append(t);
             } else {
                 std::cout << "Error: please login before attempting other transaction" << std::endl;
             }
